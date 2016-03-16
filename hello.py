ada4422f70c8859ed97193a9f70adfea3e520bcd
PrinceWangR <lightingprince@qq.com>
2016-03-16 22:02:57 +0800

    hello.py: the first py

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#name = input('Please enter your name:')
+#name = input('请输入你的名字:')
+#print("hello",name)
+
+#s1 = 72
+#s2 = 85
+#s = 100*(s2 - s1)/s1
+#print('Grade growth rate:%.1f%%' % s)
